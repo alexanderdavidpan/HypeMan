823373f7235db3f00c28b46793dfd1008a58e502
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:24:56 -0700

    Add two tests for validation of legit artist email - Add a non-legit email which should not be accepted - Add a legit email which should pass the validation

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -8,4 +8,8 @@ describe Artist do
   it { should validate_uniqueness_of(:name) }
 
   it { should validate_presence_of(:name) }
+
+  it { should allow_value('username@gmail.com').for(:email) }
+
+  it { should_not allow_value('username@incomplete').for(:email) }
 end
