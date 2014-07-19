662960c66e643f51db7dd84cac51588cd753da7d
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:28:10 -0700

    Add tests for Artist and User email validations - Add tests for email validations of uniquess - Add tests for email validations of presence - Tests were added to Artist and User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,10 @@ describe User do
 
   it { should validate_presence_of(:username) }
 
+  it { should validate_uniqueness_of(:email) }
+
+  it { should validate_presence_of(:email) }
+
   it { should allow_value('username@gmail.com').for(:email) }
 
   it { should_not allow_value('username@incomplete').for(:email) }
