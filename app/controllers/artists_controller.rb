0fb2cac901876aca2a474419b137fd05f8d03645
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-19 01:32:41 -0700

    Add Artist create path and strong params - Strong params allow name, email, and password for Artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,14 @@
 class ArtistsController < ApplicationController
   protect_from_forgery
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to root_path
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :email, :password)
+  end
 end
