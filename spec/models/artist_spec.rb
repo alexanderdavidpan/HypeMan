6b96b5184aea6b3e75acb9f3edd0cc46a4b43976
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:21:40 -0700

    Add two validation tests for Artist name - Add test for presence validation of artist name - Add test for uniqueness validation of artist name

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,4 +4,8 @@ describe Artist do
   it { should have_secure_password }
 
   it { should have_many(:songs) }
+
+  it { should validate_uniqueness_of(:name) }
+
+  it { should validate_presence_of(:name) }
 end
