48a5175cd9fbc144ece2edd372201dc202c01a51
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:16:49 -0700

    Add should in front of rspec evaluations - Shoulda matchers should of had should in front

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe User do
   it { should have_secure_password }
 
-  it { validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:username) }
 
-  it { validate_presence_of(:username) }
+  it { should validate_presence_of(:username) }
 
   it { should allow_value('username@gmail.com').for(:email) }
-  
+
   it { should_not allow_value('username@incomplete').for(:email) }
 end
