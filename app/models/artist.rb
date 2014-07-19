dadc320dea1788c51eb1f833b612a1015a6ee261
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:27:17 -0700

    Add uniqueness and presence validation to email for User and Artists - This is to prevent Users and Artists from making duplicate accounts as a User or Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ActiveRecord::Base
   has_secure_password
   has_many :songs
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :songs, through: :favorites
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
