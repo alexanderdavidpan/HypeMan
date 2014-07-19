681440a95045a71ed763a858a8d03cee8157a286
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-18 23:14:29 -0700

    Add two tests for email validation for User model - Test for valid and invalid email addresses using shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ describe User do
   it { validate_uniqueness_of(:username) }
 
   it { validate_presence_of(:username) }
+
+  it { should allow_value('username@gmail.com').for(:email) }
+  
+  it { should_not allow_value('username@incomplete').for(:email) }
 end
