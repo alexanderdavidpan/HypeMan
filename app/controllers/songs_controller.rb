301db2e64ac3e97871bff8eff6fa5e43f6e7d98d
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-19 11:06:38 -0700

    Add upload method for Songs in Song controller - Error handling for errors in song upload as well - If successful, it will redirect to root path

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,12 @@ class SongsController < ApplicationController
   end
 
   def upload
-
+    begin
+      AWS::S3::S3Object.store(sanitize_filename(params[:mp3file].original_filename), params[:mp3file].read, BUCKET, :access => :public_read)
+      redirect_to root_path
+    rescue
+      render :text => "Couldn't complete the upload"
+    end
   end
 
   def delete
@@ -19,7 +24,7 @@ class SongsController < ApplicationController
   private
  
   def sanitize_filename(file_name)
-      just_filename = File.basename(file_name)
-      just_filename.sub(/[^\w\.\-]/,'_')
+    just_filename = File.basename(file_name)
+    just_filename.sub(/[^\w\.\-]/,'_')
   end
 end
