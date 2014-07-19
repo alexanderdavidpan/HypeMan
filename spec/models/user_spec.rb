b69fc58b4e6848fcc158dbd77c1ee924547dfcf9
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-19 02:25:18 -0700

    Add validation test for User model - Users have many favorites

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe User do
   it { should have_secure_password }
 
+  it { should have_many(:favorites)}
+
   it { should validate_uniqueness_of(:username) }
 
   it { should validate_presence_of(:username) }
