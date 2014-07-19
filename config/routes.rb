a36c38d4609eb5f88e6de50d9a0007489cb5707b
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-19 00:35:38 -0700

    Add resources routes for Users and Artists - Will delete unused routes later

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  resources :users
+  resources :artists
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
