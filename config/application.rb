76b4fbdc404f923fd37cca419ef6d6693b758a8a
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-20 10:52:15 -0700

    Change configuration for AWS connection using aws-sdk gem - Syntax is a bit different but secret data is still stored in the ENV

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module HypeMan
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    AWS::S3::Base.establish_connection!(
+    AWS.config(
         :access_key_id     => ENV["aws_access_key_id"],
         :secret_access_key => ENV["aws_secret_access_key"]
     )
